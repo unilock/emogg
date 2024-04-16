bcbe8e77da7e91785298cd78d642031ab0548dc3
unilock <unilock@fennet.rentals>
2024-04-15 20:06:11 -0400

    remove update checker
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/io/github/aratakileo/emogg/Emogg.java
+++ src/main/java/io/github/aratakileo/emogg/Emogg.java
@@ -1,9 +1,7 @@
 package io.github.aratakileo.emogg;
 
-import io.github.aratakileo.emogg.api.ModrinthApi;
 import io.github.aratakileo.emogg.emoji.EmojiManager;
 import io.github.aratakileo.emogg.gui.EmojiSuggestion;
-import io.github.aratakileo.emogg.util.Platform;
 import io.github.aratakileo.suggestionsapi.SuggestionsAPI;
 import io.github.aratakileo.suggestionsapi.injector.Injector;
 import io.github.aratakileo.suggestionsapi.util.Cast;
@@ -11,7 +9,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.SharedConstants;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
@@ -30,8 +27,6 @@ public class Emogg implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ModrinthApi.checkUpdates();
-
         SuggestionsAPI.registerInjector(Injector.simple(
                 Pattern.compile("[:：][A-Za-z0-9_]*([:：])?$"),
                 (currentExpression, startOffset) -> {
@@ -43,29 +38,6 @@ public class Emogg implements ClientModInitializer {
                    return Cast.of(suggestions);
                 }
         ));
-
-        LOGGER.info(String.format(
-                "[emogg] Installed v%s; %s to download (from modrinth.com)%s",
-                Platform.getModVersion(NAMESPACE_OR_ID),
-                switch (ModrinthApi.getResponseCode()) {
-                    case SUCCESSFUL, NEEDS_TO_BE_UPDATED -> "available";
-                    default -> "not available";
-                },
-                switch (ModrinthApi.getResponseCode()) {
-                    case DOES_NOT_EXIST_AT_MODRINTH -> ", because does not exist for Minecraft v"
-                            + SharedConstants.getCurrentVersion().getName();
-                    case SUCCESSFUL, NEEDS_TO_BE_UPDATED -> String.format(
-                            " v%s - %s",
-                            ModrinthApi.getUpdateVersion(),
-                            ModrinthApi.getResponseCode() == ModrinthApi.ResponseCode.NEEDS_TO_BE_UPDATED
-                                    ? "needs to be updated" : "not needs to be updated"
-                    );
-                    default -> ", because something went wrong";
-                }
-        ));
-
-        if (ModrinthApi.getResponseCode() == ModrinthApi.ResponseCode.DOES_NOT_EXIST_AT_MODRINTH)
-            LOGGER.warn("[emogg] It looks like you are using an unofficial version port!");
 
         EmoggConfig.load();
         EmojiManager.init();
