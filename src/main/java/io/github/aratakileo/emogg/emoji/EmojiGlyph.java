f1de9186e1e2efc995dbfd8414a2c10d38377c73
unilock <unilock@fennet.rentals>
2024-04-15 20:21:23 -0400

    EmojiGlyph: EmoggRenderTypes.Shaders.Uni -> EmoggRenderTypes.Shaders.Uniform
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/io/github/aratakileo/emogg/emoji/EmojiGlyph.java
+++ src/main/java/io/github/aratakileo/emogg/emoji/EmojiGlyph.java
@@ -176,7 +176,7 @@ public abstract class EmojiGlyph extends BakedGlyph implements GlyphInfo {
         @Override
         protected void renderImpl(VertexConsumer builder, Matrix4f mat, float r, float g, float b, float a, int packedLightCoords) {
             final int ANIMATION_LENGTH = 2000;
-            EmoggRenderTypes.Shaders.Uni.loadingAnimationTime.set(
+            EmoggRenderTypes.Shaders.Uniform.loadingAnimationTime.set(
                     (float) (Util.getMillis() % ANIMATION_LENGTH) / ANIMATION_LENGTH
             );
             super.renderImpl(builder, mat, r, g, b, a, packedLightCoords);
